c5e1ed33df3ac193ba5c0c586eb753bd988bc0ed
Obito770 <144083670+Obito770@users.noreply.github.com>
2024-03-19 20:22:33 +0200

    Update اغنيه.js

--- plugins/اغنيه.js
+++ plugins/اغنيه.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 let handler = async (m, { conn, command, args, text, usedPrefix }) => {
 let q, v, yt, dl_url, ttl, size, lolhuman, lolh, n, n2, n3, n4, cap, qu, currentQuality   
-if (!text) throw `*🤔Que esta buscado? 🤔*\n*Ingrese el nombre del la canción*\n\n*Ejemplo:*\n#play emilia 420`
+if (!text) throw `*🤔ما الذي تبحث عنه؟ 🤔*\n*أدخل اسم الأغنية*\n\n*مثل:*\n.${usedPrefix + command} بابلو`
 try {
 const yt_play = await search(args.join(" "))
 let additionalText = ''

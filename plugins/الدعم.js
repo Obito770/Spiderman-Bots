08c32524dd6b94a62b950668d2d0c1aec1ab2019
Obito770 <144083670+Obito770@users.noreply.github.com>
2024-03-19 18:44:58 +0200

    Add files via upload

---
+++ plugins/الدعم.js
@@ -0,0 +1,15 @@
+let handler = async m => m.reply(`┗═──═⊱⬩⊰🩸⊱⬩⊰═──═┛
+• *تم تطويري وبرمجتي* •⊱
+• *بواسطة هــيـرو*
+*رقم المطور اذا واجهتك مشكله في البوت*:
+https://wa.me/201144148194
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+*ڪروب الدعم لـلـبـوت*
+https://chat.whatsapp.com/FZrtNMezysq24t3GPt6oxE
+┗═────═⊱⬩⊰🩸⊱⬩⊰═────═┛
+`.trim())
+handler.help = ['gpguru']
+handler.tags = ['main']
+handler.command = ['group', 'support','الدعم','دعم']
+
+export default handler
